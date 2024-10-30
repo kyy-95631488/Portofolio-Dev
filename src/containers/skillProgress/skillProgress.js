7ec07115f9285674a0e9e592df29a916baf61c0d
kyy-95631488 <riskir5622@gmail.com>
2024-10-31 03:48:19 +0700

    Update 31 10 2024 0348 Fixed

--- src/containers/skillProgress/skillProgress.js
+++ src/containers/skillProgress/skillProgress.js
@@ -2,34 +2,34 @@ import React from "react";
 import "./Progress.css";
 import { techStack } from "../../portfolio";
 import { Fade } from "react-reveal";
+import skillImage from "../../assests/images/skill.svg"; // Import the image statically
 
 export default function StackProgress(_props) {
     if (techStack.viewSkillBars) {
         return (
             <Fade bottom duration={2000} distance="40px">
-            <div className="skills-container">
-                <div className="skills-bar">
-                    <h1 className="skills-heading">Proficiency</h1>
-                    {techStack.experience.map((exp) => {
-                        const progressStyle = {
-                            width: exp.progressPercentage
-                        };
-                        return (
-                        <div className="skill">
-                            <p>{exp.Stack}</p>
-                            <div className="meter">
-                                <span style={progressStyle}></span>
-                            </div>
-                        </div>
-                        );
-                    })}
-                </div>
+                <div className="skills-container">
+                    <div className="skills-bar">
+                        <h1 className="skills-heading">Proficiency</h1>
+                        {techStack.experience.map((exp, index) => {
+                            const progressStyle = {
+                                width: exp.progressPercentage
+                            };
+                            return (
+                                <div className="skill" key={index}>
+                                    <p>{exp.Stack}</p>
+                                    <div className="meter">
+                                        <span style={progressStyle}></span>
+                                    </div>
+                                </div>
+                            );
+                        })}
+                    </div>
 
-                <div className="skills-image">
-                    <img alt="Skills" src={require("../../assests/images/skill.svg")} />
+                    <div className="skills-image">
+                        <img alt="Skills" src={skillImage} />
+                    </div>
                 </div>
-
-            </div>
             </Fade>
         );
     }
